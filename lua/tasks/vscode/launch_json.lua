774535b68bf10f8215d6bcd3f24b0fb3ae7b5ee8
Tom Lynch <twhlynch@gmail.com>
2026-05-10 00:13:34 +1000

    feat: implement launch tasks printing the command to run

--- lua/tasks/vscode/launch_json.lua
+++ lua/tasks/vscode/launch_json.lua
@@ -1,4 +1,5 @@
 local utils = require("tasks.utils")
+local consts = require("tasks.vscode.consts")
 
 local M = {}
 
@@ -11,9 +12,37 @@ function M.tasks(bufnr)
 		return {}
 	end
 
-	vim.notify(vim.inspect(json))
+	if json.version ~= consts.launch_version then
+		---@diagnostic disable-next-line: redundant-parameter
+		vim.notify(vim.fn.printf(consts.strings.bad_version, json.version, consts.launch_version), vim.log.levels.WARN)
+	end
 
-	return {}
+	if json.configurations == nil or type(json.configurations) ~= "table" then
+		return {}
+	end
+
+	local tasks = {}
+
+	for _, task in ipairs(json.configurations) do
+		if task.name then
+			local lnum = utils.find_line(bufnr, "name", task.name)
+			if lnum then
+				tasks[#tasks + 1] = {
+					lnum = lnum,
+					run = function()
+						M.run(task)
+					end,
+				}
+			end
+		end
+	end
+
+	return tasks
+end
+
+--- @param task vscode.LaunchConfig
+function M.run(task)
+	vim.notify(vim.inspect(task))
 end
 
 return M
