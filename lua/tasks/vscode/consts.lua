c5da321c44179ba356b5efa1f645f0ad86ea085c
Tom Lynch <twhlynch@gmail.com>
2026-05-10 00:47:41 +1000

    feat: utils for running vscode tasks

--- lua/tasks/vscode/consts.lua
+++ lua/tasks/vscode/consts.lua
@@ -3,6 +3,11 @@ return {
 	launch_version = "0.2.0",
 
 	strings = {
+		missing_command = "Config missing 'command'",
+		missing_script = "Config missing 'script'",
+		missing_program = "Config missing 'program' or 'runtimeExecutable'",
+		task_not_found = "preLaunchTask '%s' not found in tasks.json",
 		bad_version = "Version %s is not %s and may not be supported",
+		no_default_build_task = "No default build task found",
 	},
 }
---
+++ lua/tasks/vscode/utils.lua
@@ -0,0 +1,99 @@
+local M = {}
+
+--- build a command string or array from a command and args
+--- @param command string
+--- @param args? string[]
+--- @param inputs vscode.UserInput
+--- @param env env
+--- @return command
+function M.build_cmd(command, args, inputs, env)
+	local cmd = { M.resolve_vars(command, inputs, env) }
+
+	if args == nil or #args == 0 then
+		-- just the command as a string
+		return cmd[1]
+	else
+		-- add each argument
+		for _, arg in ipairs(args) do
+			table.insert(cmd, M.resolve_vars(arg, inputs, env))
+		end
+	end
+
+	-- list of command and args
+	return cmd
+end
+
+--- resolve vscode, input, and env variables in a string
+--- @param str string
+--- @param inputs table<string, vscode.UserInput>
+--- @param env env
+--- @return string | nil
+function M.resolve_vars(str, inputs, env)
+	if not str then
+		return nil
+	end
+
+	 -- stylua: ignore
+	local replacements = {
+		["${workspaceFolder}"] =         vim.fn.getcwd(),
+		["${file}"] =                    vim.fn.expand("%:p"),
+		["${fileDirname}"] =             vim.fn.expand("%:p:h"),
+		["${fileBasename}"] =            vim.fn.expand("%:t"),
+		["${fileBasenameNoExtension}"] = vim.fn.expand("%:t:r"),
+		["${workspaceFolderBasename}"] = vim.fn.fnamemodify(vim.fn.getcwd(), ":t"),
+	}
+
+	for pattern, replacement in pairs(replacements) do
+		str = str:gsub(vim.pesc(pattern), replacement)
+	end
+
+	-- env variables
+	str = str:gsub("$([%w_]+)", function(env_var)
+		return env[env_var] or ""
+	end)
+
+	-- find an input option
+	str = str:gsub("${input:([^}]+)}", function(id)
+		local input = inputs[id]
+		if not input then
+			return ""
+		end
+
+		if input.type == "promptString" then
+			return vim.fn.input((input.description or id) .. ": ", input.default or "")
+		end
+		return ""
+	end)
+
+	return str
+end
+
+--- resolve variables in env and add to a merged env
+--- @param source_env env
+--- @param inputs table<string, vscode.UserInput>
+--- @return env
+function M.build_env(source_env, inputs)
+	---@type env
+	local merged = vim.fn.environ()
+
+	for k, v in pairs(source_env) do
+		merged[k] = M.resolve_vars(v, inputs, merged)
+	end
+
+	return merged
+end
+
+--- extract inputs into a mapping
+--- @param inputs vscode.UserInput[]
+--- @return table<string, vscode.UserInput>
+function M.extract_inputs(inputs)
+	local map = {}
+	if inputs then
+		for _, input in ipairs(inputs) do
+			map[input.id] = input
+		end
+	end
+	return map
+end
+
+return M
