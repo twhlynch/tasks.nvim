17ae58ca83b7eb02609b42ced69d4330feba65ba
Tom Lynch <twhlynch@gmail.com>
2026-05-10 00:04:14 +1000

    feat: implement tasks.json tasks printing the command to run

---
+++ lua/tasks/vscode/consts.lua
@@ -0,0 +1,8 @@
+return {
+	tasks_version = "2.0.0",
+	launch_version = "0.2.0",
+
+	strings = {
+		bad_version = "Version %s is not %s and may not be supported",
+	},
+}
--- lua/tasks/vscode/tasks_json.lua
+++ lua/tasks/vscode/tasks_json.lua
@@ -1,4 +1,5 @@
 local utils = require("tasks.utils")
+local consts = require("tasks.vscode.consts")
 
 local M = {}
 
@@ -11,9 +12,38 @@ function M.tasks(bufnr)
 		return {}
 	end
 
-	vim.notify(vim.inspect(json))
+	if json.version ~= consts.tasks_version then
+		---@diagnostic disable-next-line: redundant-parameter
+		vim.notify(vim.fn.printf(consts.strings.bad_version, json.version, consts.tasks_version), vim.log.levels.WARN)
+	end
 
-	return {}
+	if json.tasks == nil or type(json.tasks) ~= "table" then
+		return {}
+	end
+
+	local tasks = {}
+
+	for _, task in ipairs(json.tasks) do
+		if task.label then
+			local lnum = utils.find_line(bufnr, "label", task.label)
+			if lnum then
+				tasks[#tasks + 1] = {
+					lnum = lnum,
+					run = function()
+						M.run(task, json.inputs)
+					end,
+				}
+			end
+		end
+	end
+
+	return tasks
+end
+
+--- @param task vscode.TaskConfig
+--- @param inputs vscode.UserInput[]
+function M.run(task, inputs)
+	vim.notify(vim.inspect(task) .. tostring(#inputs))
 end
 
 return M
