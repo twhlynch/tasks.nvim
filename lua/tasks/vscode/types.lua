1cd8e9be83519d348ae175e024bcbdffed738d27
Tom Lynch <twhlynch@gmail.com>
2026-05-10 00:00:00 +1000

    chore: config structure types for vscode tasks

---
+++ lua/tasks/vscode/types.lua
@@ -0,0 +1,46 @@
+--- @alias command string | string[]
+--- @alias env table<string, string>
+
+--- @class vscode.TaskOptions vscode task options
+--- @field env? env map of env variable name to value that will exist for the task
+--- @field cwd? string working directory for the task to run in
+
+--- @class vscode.TaskConfig vscode task
+--- @field label? string unique name for a task
+--- @field type? "shell" | "process" | "npm" is the task a shell command or a process or other
+--- @field command? string command to run
+--- @field args? string[] args to pass to the command
+--- @field options? vscode.TaskOptions extra task options
+--- @field group? string | vscode.Group group info
+--- @field script? string script for npm tasks
+
+--- @class vscode.Group task group info
+--- @field kind string group name
+--- @field isDefault boolean if the task is the default for the group
+
+--- @class vscode.UserInput vscode task input
+--- @field id? string unique id for input
+--- @field type? "promptString" input type
+--- @field description? string prompt to show for the input
+--- @field default? string default value for the input
+
+--- @class vscode.LaunchConfig vscode launch config
+--- @field name? string unique name for launch config
+--- @field type? "node" | "python" | "debugpy" | "cppdbg" | "extensionHost" type of config
+--- @field request? "launch" | "attach" request type of config
+--- @field program? string absolute path to the program
+--- @field runtimeExecutable? string absolute path to the program
+--- @field args? string[] args to pass to the command
+--- @field preLaunchTask? string config id to run first
+--- @field cwd? string working directory for the config to run in
+--- @field env? env map of env variable name to value that will exist for the config
+
+--- @class vscode.TasksJson tasks.json file schema
+--- @field version? string schema version
+--- @field tasks? vscode.TaskConfig[] list of task congifs
+--- @field inputs? vscode.UserInput[] list of inputs
+
+--- @class vscode.LaunchJson launch.json file schema
+--- @field version? string version of schema
+--- @field configurations? vscode.LaunchConfig[] list of launch configs
+--- @field inputs? vscode.UserInput[] list of inputs
